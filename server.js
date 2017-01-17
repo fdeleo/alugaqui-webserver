cc4fa238e4fe8b18cdf6ab08b94b1488dd19fe36
Felipe De Leo <felipedeleo@gmail.com>
2017-01-17 03:32:30 -0200

    Adds search GET Request
    Adds listener to GET Requests from the search function

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ var app = express();
 *** USE CORRECT RELATIVE PATH TO THE INDEX ***
 */
 
-app.use(express.static('../alugaqui-client-web'))
+app.use(express.static('../alugaqui-client-web'));
+
+/* Adds search function responses */
+
+/* SEARCH - 1 - BAIRRO */
+app.get('/bairro/:bairro', (req, res) =>{
+  res.send(req.params);  
+});
 
 // Bind the application to a port on our machine (so it can actually work)
 app.listen(3000);
