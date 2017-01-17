622384edab4bb24b912a26b4d0079a9cdd3f5e79
Felipe De Leo <felipedeleo@gmail.com>
2017-01-17 04:51:11 -0200

    First render page implementation
    Renders the listagemImoveis dynamically

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+
+app.set('view engine', 'hbs');
 /* Load Web Server *
 *** USE CORRECT RELATIVE PATH TO THE INDEX ***
 */
-
 app.use(express.static('../alugaqui-client-web'));
 
 /* Adds search function responses */
 
 /* SEARCH - 1 - BAIRRO */
 app.get('/bairro/:bairro', (req, res) =>{
-  res.send(req.params);  
+
+  /* Render listragemImoveis pages with the GET params from the request */
+  res.render('listagemImoveis.hbs' , req.params)
 });
 
 // Bind the application to a port on our machine (so it can actually work)
