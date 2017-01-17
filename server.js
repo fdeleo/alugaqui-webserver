c624f8d22215f8c325fa9dc9021c7aaf31aff877
Felipe De Leo <felipedeleo@gmail.com>
2017-01-17 16:46:04 -0200

    Test search comparison for validation [Input vs internal data]

--- server.js
+++ server.js
@@ -15,6 +15,8 @@ app.use(express.static('../alugaqui-client-web'));
 /* SEARCH - 1 - BAIRRO */
 app.get('/bairro/:bairro', (req, res) =>{
 
+  /* Logic to search properties that have the respective parameter*/ 
+  buscaImovel(req);
   /* Render listragemImoveis pages with the GET params from the request */
   res.render('listagemImoveis.hbs' , req.params)
 });
@@ -22,4 +24,37 @@ app.get('/bairro/:bairro', (req, res) =>{
 // Bind the application to a port on our machine (so it can actually work)
 app.listen(3000);
 
+
+/* Busca Imovel */
+
+function buscaImovel(req) {
+  var paramBairro = req.params.bairro;
+  console.log("Busca do Bairro:");
+  console.log(paramBairro);
+  
+  var buscaResultado = null;
+  
+  var imovel = criaImovel();
+  
+  console.log("\n Detalhes do Imovel");
+  console.log(imovel);
+  console.log(buscaResultado);
+    
+  if ( paramBairro == imovel.bairro ) {
+    buscaResultado = imovel ;
+    console.log(buscaResultado);
+  }
+    
+  return buscaResultado;
+}
+
+function criaImovel() {
+  var imovel = {
+    bairro : "barra" ,
+    endereco : "Rua de teste para validacao 200" ,
+    cep: "00000-000"
+  };  
+  
+  return imovel ;
+}
 
