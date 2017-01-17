ad07a236dd9dbb4b9d15c1f4252eee52789ce038
Felipe De Leo <felipedeleo@gmail.com>
2017-01-17 02:19:47 -0200

    Adds Basic Web Server Config
    Adds Basic Web Server Config

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 
 var app = express();
 
-// get(url , function) => Function to execute when reaching specified URL
-app.get('/', (req, res) =>{
-  res.send('Hello Express');
-  
-}) 
+/* Load Web Server *
+*** USE CORRECT RELATIVE PATH TO THE INDEX ***
+*/
+
+app.use(express.static('../alugaqui-client-web'))
 
 // Bind the application to a port on our machine (so it can actually work)
 app.listen(3000);
