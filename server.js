5e9b6bf2e22bbed8fb28aa4323bfa385609374fd
Felipe De Leo <felipedeleo@gmail.com>
2017-01-19 16:03:36 -0200

    Changes search function directly into app.get() and SYNCHRONOUS! :trophy: :trophy:
    Now the search function is synchronous, without writing files and inside
    the same function from the request (with Callbacks).

--- server.js
+++ server.js
@@ -11,65 +11,48 @@ app.set('view engine', 'hbs');
 *** USE CORRECT RELATIVE PATH TO THE INDEX ***
 */
 app.use(express.static('../alugaqui-client-web'));
-
-/* Adds search function responses */
-
-/* SEARCH - 1 - BAIRRO */
-app.get('/bairro/:bairro', (req, res) =>{
-
-  /* Logic to search properties that have the respective parameter*/
-  // var resultado = buscaImovel(req);
-  buscaImovel(req);
-
-  fs.readFile('file.json', 'utf8' , function (err,data) {
-  if (err) {
-    return console.log(err);
-  }
-  propertyData = JSON.parse(data);
-  var result = {
-                imovel: propertyData ,
-                };
-
-  console.log('RESULTADO: \n');
-  console.log(result);
-  /* Render listragemImoveis pages with the GET params from the request */
-  res.render('listagemImoveis.hbs' , result);
-  });
-
-});
-
-// Bind the application to a port on our machine (so it can actually work)
 app.listen(3000);
 
-/* Function buscaImovel to retrieve results from DB */
-function buscaImovel(req) {
-  var paramBairro = req.params.bairro ;
+// var searchBairroUrl ;
+/* Get Request to './bairro/${neighborhood}' */
+app.get('/bairro/:neighborhood', (req, res, next) =>{
+  var paramBairro = req.params.neighborhood ;
   //var paramBairro = req.params.bairro.toLowerCase();
   var searchUrl = 'http://138.68.27.226:8080/imoveis/';
   // Add later the complement with neighborhood
   var searchBairroUrl = searchUrl + paramBairro;
-  var resultado = [];
+  console.log('Teste GET: \n');
+  console.log(req.params);
+  console.log('Neighborhood URL: \n');
+  console.log(searchBairroUrl);
 
-  var busca = request({
-    url: searchBairroUrl ,
-    json: true
-  }
-  , function (error, response, body)
-  {
-    if (!error && response.statusCode === 200) {
-          console.log('BODY: \n');
-          console.log(body); // Print the json response
-
-          // fs.writeFileSync
-
-          fs.writeFileSync('file.json', JSON.stringify(body) , 'utf8');
-          // , function(err){
-          //   if(err){console.log(err);} else {console.log("File saved successfully");}
-          // }
-
-        }
-    else{
-      return(-1);
+  request({
+      url: searchBairroUrl ,
+      json: true
     }
+    , function (error, response, body){
+      if (!error && response.statusCode === 200) {
+        console.log('BODY: \n');
+        console.log(body); // Print the json response
+        var propertyData = body;
+
+        if ( body == '[]' || body == undefined ){
+          console.log('Error: Empty JSON string');
+        }
+
+        else {
+          var result = {
+                           imovel: propertyData ,
+                        };
+          console.log('RESULTADO: \n');
+          console.log(result);
+        /* Render listagemImoveis pages with the GET params from the request */
+          res.render('listagemImoveis.hbs' , result);
+        }
+      }
+      else {
+        return(-1);
+        console.log('ERRO: \n' + error);
+      }
   });
-};
+});
